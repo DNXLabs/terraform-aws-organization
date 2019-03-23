45e9fc5858a43c8289f341eb26278f180fec9a19
Allan Denot <adenot@gmail.com>
2019-03-23 18:23:33 +1100

    Adding variable to store the org name and simpler output names

--- _outputs.tf
+++ _outputs.tf
@@ -1,9 +1,13 @@
-output "organization_id" {
+output "id" {
   value       = "${aws_organizations_organization.org.id}"
   description = "ID of organization created"
 }
 
-output "organization_root_id" {
+output "root_id" {
   value       = "${data.external.organizations_roots.result.id}"
   description = "ID of the root Organizational Unit of the organization created"
+}
+
+output "name" {
+  value = "${var.name}"
 }
---
+++ _variables.tf
@@ -0,0 +1,3 @@
+variable "name" {
+  description = "Name for this organization (not actually used in API call)"
+}
